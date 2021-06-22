758a958238baf843c31acc405fb9acc1d94a8e1e
Your Name <jelmerkleine@gmail.com>
2021-06-22 17:27:29 +0200

    addded lidar to the urdf

--- scripts/start.py
+++ scripts/start.py
@@ -11,26 +11,24 @@ from std_msgs.msg import Float64
 def main():
     rospy.init_node('car', anonymous=True) #make node
    
-    while not rospy.is_shutdown():    
-         driveVehicle() 
+    while not rospy.is_shutdown(): 
+        speed = 1
+        steer = 0
+        driveVehicle(speed,steer) 
 
-def driveVehicle():
+def driveVehicle(vel_cmd,steer_cmd):
     wheel_rear_right = rospy.Publisher('car/rr_wheel_controller/command', Float64, queue_size=10)
     wheel_rear_left = rospy.Publisher('car/rl_wheel_controller/command', Float64, queue_size=10)
     steer_front = rospy.Publisher('/car/fl_steer_controller/command', Float64, queue_size=10)
     steer_rear = rospy.Publisher('/car/fr_steer_controller/command', Float64, queue_size=10)
-    rate = rospy.Rate(10) # 100hz
-    #while not rospy.is_shutdown():
     
-    print("hallo")
-    vel_cmd = 200
-    steer_cmd= 20000
-    rospy.loginfo(vel_cmd)
-    rospy.loginfo(steer_cmd)
+    #rospy.loginfo(vel_cmd)
+    #rospy.loginfo(steer_cmd)
     wheel_rear_right.publish(vel_cmd)
     wheel_rear_left.publish(vel_cmd)
     steer_front.publish(steer_cmd)
     steer_rear.publish(steer_cmd)
+    rate = rospy.Rate(10) # 100hz
     rate.sleep()
 
 if __name__ == "__main__":
