bd0deca8b41935e0a3d29fe716e9450e2943055f
Jesper Venneman <jesper.venneman@gmail.com>
2021-06-26 18:27:24 +0200

    added parking

--- scripts/start.py
+++ scripts/start.py
@@ -12,12 +12,48 @@ from std_msgs.msg import Float32
 from sensor_msgs.msg import LaserScan
 import sensor_msgs.msg
 
+# Vehicle Properties (From vehicle source)
+baseLinkOffset = [0, -1]
+
+# overall length of the vehicle (meters)
+length = 4.88
+
+# overall wheel base of the vehicle (meters)
+wheel_base = 2.85
+# overhang
+overh = (length - wheel_base)/2
+
+# overall width
+#width = 1.920
+width = 1.910
+
+# maximum steering angle (radians)
+beta_max = 0.6
+
+# minimum turning radius (meters)
+# R_min = overh/math.sin(beta_max)
+R_min = 5
+
+# inner turning radius - minimum
+Ri_min = np.sqrt(R_min**2 - wheel_base**2) - (width/2)
+
+# outer turning radius - minimum
+Re_min = np.sqrt((Ri_min+width)**2 + (wheel_base+overh)**2)
+
+# minimum length of the parking space required - 1 shot
+L_min = overh + np.sqrt(Re_min**2 - Ri_min**2)
+
+# Variables
 scan = Float32()
 scanIncrement = Float32()
-currentVehiclePos = [Float64(), Float64()]
 validCoordinatesBack = []
 validCoordinatesFront = []
-baseLinkOffset = [0, -1]
+
+
+startPos = [0, 0]
+currentVehiclePos = [Float64(), Float64()]
+goalPos = [0, 0]
+flag = 0
 
 def getCurrentVehiclePos():
     return [currentVehiclePos[0].data, currentVehiclePos[1].data]
@@ -137,21 +173,55 @@ def getObstacles():  # Obstacle 1 is behind car, obstacle 2 is in front of car
     return obstacle1, obstacle2
 
 
+def calcTransition(obstacle1, obstacle2):  # Calculate the transition for parking
+    r_prime = Ri_min + width/2
+    
+    c1 = [goalPos[0], goalPos[1] + r_prime]
+    y_c2 = startPos[1] - r_prime
+
+    pt = [
+        (c1[1] + y_c2) / 2,
+        0
+    ]
+
+    pt[1] = c1[1] + np.sqrt(r_prime**2 - (pt[0] - c1[0])**2)
+    return pt
+
+
+def parkVehicle(start_pos, transition_pos, goal_pos, update_pos):
+    if update_pos[0]>start_pos[0]:
+        vel = -12
+        steer = 0
+        
+    elif update_pos[0]<=(start_pos[0]) and update_pos[0]>(transition_pos[0]):
+        vel = -104
+        steer = -0.49
+
+    elif update_pos[0]<=(transition_pos[0]) and update_pos[0]>(goal_pos[0]-0.5) and flag == 0:
+        steer = 0.49
+        vel = -104
+    elif update_pos[0]<=goal_pos[0]-0.5:
+        steer = 0
+        vel = 20
+        global flag
+        flag = 1
+        
+    elif flag == 1:
+        steer = 0
+        vel = -5
+
+    return vel, steer  
+
+
 def main():
     rospy.init_node('car', anonymous=True) #make node
     obstacle1 = 0
     obstacle2 = 0
-    startPos = [0, 0]
 
 
     done = False
     while not rospy.is_shutdown():
         #print("Moving")
-        speed = 0
-        steer = 0
-        driveVehicle(speed, steer)
-        print(getCurrentVehiclePos())
-        print(startPos)
         if ((obstacle1 == 0) or (obstacle2 == 0) and not done):
             done = True
             findObstacles(scan.data)
@@ -160,10 +230,19 @@ def main():
             print("obstacle2", obstacle2)
             startPos = getCurrentVehiclePos()
             print("startPos", startPos)
+            goalPos = [(obstacle2[1][1] - obstacle1[1][1]) / 2, (obstacle1[1][0] - obstacle1[0][0]) / 2]
+            print("goalPos", goalPos)
+            print("currtentPos", getCurrentVehiclePos())
+        else:
+            continue
+        velocity, steer = parkVehicle(startPos, calcTransition(obstacle1, obstacle2), goalPos, getCurrentVehiclePos())
+        driveVehicle(velocity, steer)
+
             
         
 
 def driveVehicle(vel_cmd,steer_cmd):
+    print("Moving at velocity: ", vel_cmd, "and steer: ", steer_cmd, "!")
     wheel_rear_right = rospy.Publisher('/car/rr_wheel_controller/command', Float64, queue_size=10)
     wheel_rear_left = rospy.Publisher('/car/rl_wheel_controller/command', Float64, queue_size=10)
     steer_front = rospy.Publisher('/car/fl_steer_controller/command', Float64, queue_size=10)
